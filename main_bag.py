726f4dbbeef1539b2418bf5312664fb3857a2a9c
sean-hackett <seanhackett55@gmail.com>
2019-07-03 09:32:41 -0400

    Remove landmark, change camera #

--- main_bag.py
+++ main_bag.py
@@ -7,11 +7,11 @@ from rospy_msg_converter import convert_ros_message_to_dictionary
 WINDOW_DURATION = .1
 
 def main():
-    keep = ['gazeAngle', 'gazeDirection0', 'gazeDirection1', 'landmarks', 'head_pose', ]
+    keep = ['gazeAngle', 'gazeDirection0', 'gazeDirection1', 'head_pose', ]
     with source.ROSbag(
         lambda m: {key: m[key] for key in keep},
         filename='bag.bag',
-        topic='/camera2/face_info') as face_source, \
+        topic='/camera1/face_info') as face_source, \
          sink.CSV(WINDOW_DURATION, 'out.csv') as face_sink:
 
         feature_extractor.face(face_source, face_sink)
